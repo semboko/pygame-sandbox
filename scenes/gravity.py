9179e362ef4278bb0122f8dde8871ad8d3c5b92e
Simon Bondarenko <bondarenko@vt.edu>
2023-02-17 06:14:03 -0600

    Code formatting

--- scenes/gravity.py
+++ scenes/gravity.py
@@ -1,13 +1,17 @@
 from typing import Tuple
-from .utils import convert
+
+import pygame.draw
 import pymunk
 from pygame.event import Event
 from pygame.surface import Surface
-import pygame.draw
+
 from scenes.abstract import AbstractScene
 
+from .utils import convert
+
+
 class Ball:
-    def __init__(self, x:int, y:int, r:int, space: pymunk.Space, btype: int = pymunk.Body.DYNAMIC):
+    def __init__(self, x: int, y: int, r: int, space: pymunk.Space, btype: int = pymunk.Body.DYNAMIC):
         self.body = pymunk.Body(body_type=btype)
         self.body.position = x, y
         self.shape = pymunk.Circle(self.body, r)
@@ -15,34 +19,40 @@ class Ball:
         self.r = r
         self.debug = space.debug_draw
         space.add(self.body, self.shape)
+
     def render(self, display: Surface) -> None:
         h = display.get_height()
         pygame.draw.circle(display, (244, 0, 0), convert(self.body.position, h), self.r)
         if self.debug:
-            print(f'circle by id {id(self)}: angle = {self.body.angle}, pos = {tuple(self.body.position)}')
+            print(f"circle by id {id(self)}: angle = {self.body.angle}, pos = {tuple(self.body.position)}")
+
 
 class Segment:
-    def __init__(self, a: Tuple[int, int], b: Tuple[int, int], r: int, space: pymunk.Space, btype: int = pymunk.Body.DYNAMIC):
+    def __init__(
+        self, a: Tuple[int, int], b: Tuple[int, int], r: int, space: pymunk.Space, btype: int = pymunk.Body.DYNAMIC
+    ):
         self.body = pymunk.Body(body_type=btype)
         self.shape = pymunk.Segment(self.body, a, b, r)
         self.r = r
         space.add(self.body, self.shape)
+
     def render(self, display: Surface) -> None:
         h = display.get_height()
         pygame.draw.line(display, (0, 0, 0), convert(self.shape.a, h), convert(self.shape.b, h), self.r)
 
+
 class GravityScene(AbstractScene):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        
+
         self.space: pymunk.Space() = pymunk.Space()
         self.space.gravity = 0, -1000
         self.space.debug_draw = True
-        self.space.damping = 0.5 # Set the friction coefficient of the space object
+        self.space.damping = 0.5  # Set the friction coefficient of the space object
         self.circle = Ball(250, 400, 10, self.space)
         self.pause = False
 
-        self.segment = Segment((0, 100),(500, 50),5,self.space,btype = pymunk.Body.KINEMATIC)
+        self.segment = Segment((0, 100), (500, 50), 5, self.space, btype=pymunk.Body.KINEMATIC)
 
     def handle_events(self, events: Tuple[Event]) -> None:
         for event in events:
@@ -61,7 +71,7 @@ class GravityScene(AbstractScene):
 
     def update(self):
         if not self.pause:
-            self.space.step(1/self.fps)
+            self.space.step(1 / self.fps)
 
     def render(self):
         self.display.fill((255, 255, 255))
