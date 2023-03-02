73d917ad3612825e18f76e17aab7aacf38e9f14f
Simon Bondarenko <bondarenko@vt.edu>
2023-03-01 22:01:56 -0600

    Added gun

--- scenes/components/tank.py
+++ scenes/components/tank.py
@@ -2,9 +2,10 @@ from typing import List, Sequence
 
 import pygame
 import pymunk
+from math import cos, sin
 from pygame import draw
 from pygame.surface import Surface
-from pymunk import Body, GearJoint, PivotJoint, Poly, Shape, ShapeFilter, SimpleMotor, Space
+from pymunk import Body, GearJoint, PivotJoint, Poly, Shape, ShapeFilter, SimpleMotor, Space, DampedRotarySpring, RatchetJoint, RotaryLimitJoint
 
 from scenes.components.ball import Ball
 from scenes.components.rect import Rect
@@ -19,15 +20,20 @@ class Tank:
     turret_shape: Shape
 
     def __init__(self, x, y, width, height, space: Space):
+        self.width = width
+        self.height = height
         self.collision_filter = ShapeFilter(group=0b1)
 
         self.add_wheel_holder(x, y - height // 4, width, height // 2, space)
         self.add_wheels(x, y, width, height, space)
-        self.add_turret(x, y, width, height, space)
+        self.add_turret(width, height, space)
+        self.add_gun(x, y, width, height, space)
+        self.add_bullet()
         self.add_motor(space)
 
     def add_wheel_holder(self, x, y, width: int, height: int, space: Space):
-        self.wheel_holder = Rect(x, y, width, height, space)
+        self.wheel_holder = Rect(x, y, width, height, space, color=(60, 163, 31))
+        self.wheel_holder.body.mass = 10000
         self.wheel_holder.shape.filter = self.collision_filter
 
     def add_wheels(self, x: int, y: int, body_width: int, body_height: int, space: Space):
@@ -53,19 +59,48 @@ class Tank:
         self.motor = SimpleMotor(self.wheel_holder.body, self.wheels[-1].body, 0)
         space.add(self.motor)
 
-    def add_turret(self, x, y, width, height, space: Space):
+    def add_turret(self, width, height, space: Space):
         vertices = (
-            (x - width // 4, y),
-            (x + width // 4, y),
-            (x + width // 4, y + height // 2),
-            (x - width // 4, y + height // 2),
+            (-width // 4, height // 4),
+            (width // 4, height // 4),
+            (width // 4, height // 1.5),
+            (-width // 4, height // 1.5),
         )
         self.turret_shape = Poly(self.wheel_holder.body, vertices)
         self.turret_shape.filter = self.collision_filter
         space.add(self.turret_shape)
 
     def add_gun(self, x, y, width, height, space: Space):
-        pass
+        self.gun = Rect(x + width//2, y + height//4, width, 5, space, color=(49, 122, 28))
+        self.gun.body.mass = 20
+        self.gun.shape.filter = self.collision_filter
+        space.add(PivotJoint(self.gun.body, self.wheel_holder.body, (-width//2, 2), (0, height//2)))
+        self.gun_joint = RotaryLimitJoint(self.gun.body, self.wheel_holder.body, -.2, 0)
+        space.add(self.gun_joint)
+
+    def add_bullet(self):
+        space = self.wheel_holder.body.space
+        width, height = self.width, self.height
+        x, y = self.wheel_holder.body.position
+        self.bullet = Ball(x, y + height//4, 4, space)
+        self.bullet.shape.elasticity = .1
+        self.bullet.body.mass = 400
+        self.bullet_holder = PivotJoint(self.gun.body, self.bullet.body, (width//2, 0), (0, 0))
+        space.add(self.bullet_holder)
+        self.bullet.shape.filter = self.collision_filter
+
+    def shot(self) -> Ball:
+        space = self.bullet.body.space
+        space.remove(self.bullet_holder)
+        r = self.bullet.shape.radius
+        x = r * cos(self.gun.body.angle)
+        y = r * sin(self.gun.body.angle)
+        force = (x * 10000000, y * 10000000)
+        self.bullet.body.apply_force_at_local_point(force, (0, 0))
+        self.wheel_holder.body.apply_force_at_local_point((-force[0]*5, -force[1]*5), (0, 0))
+        prev_bullet = self.bullet
+        self.add_bullet()
+        return prev_bullet
 
     def update_velocity(self, keys: Sequence[bool]):
         if keys[pygame.K_d]:
@@ -77,15 +112,25 @@ class Tank:
 
         self.motor.rate *= 0.8
 
+    def update_gun_angle(self, keys: Sequence[bool]):
+        if keys[pygame.K_UP]:
+            self.gun_joint.min -= .05
+            self.gun_joint.max -= .05
+        if keys[pygame.K_DOWN]:
+            self.gun_joint.min += .05
+            self.gun_joint.max += .05
+
     def update(self):
         keys = pygame.key.get_pressed()
         self.update_velocity(keys)
+        self.update_gun_angle(keys)
 
     def render(self, display: Surface):
         self.wheel_holder.render(display)
-
-        # turret_vert = [convert(self.body.local_to_world(v), h) for v in self.turret_shape.get_vertices()]
-        # draw.polygon(display, (212, 212, 212), turret_vert)
-
+        h = display.get_height()
+        turret_vert = [convert(self.turret_shape.body.local_to_world(v), h) for v in self.turret_shape.get_vertices()]
+        draw.polygon(display, (60, 163, 31), turret_vert)
+        self.gun.render(display)
+        self.bullet.render(display)
         for wheel in self.wheels:
             wheel.render(display)
--- scenes/tank.py
+++ scenes/tank.py
@@ -5,7 +5,9 @@ from pymunk import Body
 from scenes.abstract import AbstractPymunkScene
 from scenes.components.random_floor import RandomFloor
 from scenes.components.segment import Segment
+from scenes.components.ball import Ball
 from scenes.components.tank import Tank
+from scenes.utils import convert
 
 
 class TankScene(AbstractPymunkScene):
@@ -15,7 +17,7 @@ class TankScene(AbstractPymunkScene):
     def reset_scene(self):
         super().reset_scene()
         self.tank = Tank(250, 160, 130, 50, self.space)
-        self.floor = RandomFloor(0, self.display.get_width(), 0, 70, 20, self.space)
+        self.floor = RandomFloor(0, self.display.get_width(), 0, 80, 30, self.space)
         self.objects.extend((self.tank, self.floor))
 
     def update(self):
@@ -24,5 +26,17 @@ class TankScene(AbstractPymunkScene):
 
     def handle_event(self, event: Event) -> None:
         if event.type == pygame.KEYDOWN:
-            if chr(event.key) == "r":
+            if event.key == ord("r"):
                 self.reset_scene()
+
+            # Whitespace
+            if event.key == 32:
+                bullet = self.tank.shot()
+                self.objects.append(bullet)
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            h = self.display.get_height()
+            x, y = event.dict["pos"]
+            ball = Ball(*convert((x, y), h), 40, self.space)
+            self.objects.append(ball)
+
